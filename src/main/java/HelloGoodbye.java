86dba8076ec57735814cbd08f759f36398a36995
ravisk <ravisinghkushwaha@gmail.com>
2022-05-12 08:07:41 +0530

    Changed Logic in HelloGoodbye from array to variable

--- src/main/java/HelloGoodbye.java
+++ src/main/java/HelloGoodbye.java
@@ -3,12 +3,16 @@ import java.util.Scanner;
 public class HelloGoodbye {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] name = new String[2];
+
+        String name1 = sc.nextLine();
+        String name2 = sc.nextLine();
+
+     /*   String[] name = new String[2];
 
         name[0] = sc.next();
-        name[1] = sc.next();
+        name[1] = sc.next();   */
 
-        System.out.println("Hello " + name[0] + " and " + name[1]);
-        System.out.println("Goodbye " + name[1] + " and " + name[0]);
+        System.out.println("Hello " + name1 + " and " + name2);
+        System.out.println("Goodbye " + name2 + " and " + name1);
     }
 }
