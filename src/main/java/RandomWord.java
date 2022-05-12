9bbf72e884f29cce909962414de2a3eb9ec804a7
ravisk <ravisinghkushwaha@gmail.com>
2022-05-12 07:50:13 +0530

    RandomWord Bernaulli

--- src/main/java/RandomWord.java
+++ src/main/java/RandomWord.java
@@ -1,3 +1,33 @@
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
 public class RandomWord {
 
+    public static void main(String[] args) {
+
+        String challenger = "";
+        String champ = StdIn.readString();
+        StdOut.print("Initial Champ:");
+        StdOut.println(champ);
+
+        int count = 1;
+        while (!StdIn.isEmpty()) {
+            count++;
+            challenger = StdIn.readString();
+            StdOut.print("A new Challenger approaches:  ");
+            StdOut.println(champ);
+
+            if (StdRandom.bernoulli((1.0/count))) {
+                champ = challenger;
+                StdOut.println("Yes, Change the champ to the challenger");
+            } else {
+                StdOut.println("No, the champ should remain unchanged");
+            }
+            StdOut.print("Current champ: ");
+            StdOut.println(champ);
+        }
+        StdOut.println(champ);
+    }
 }
